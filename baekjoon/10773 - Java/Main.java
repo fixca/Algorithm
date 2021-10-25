9a5784a40f9f4289a19010e6e4ddf80c90ba3a04
fixca <41466407+fixca@users.noreply.github.com>
2021-10-25 14:43:47 +0900

    Update Main.java

--- baekjoon/10773 - Java/Main.java
+++ baekjoon/10773 - Java/Main.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.Stack;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Integer> stack = new Stack<>();
+        int K = Integer.parseInt(br.readLine());
+        for(int i = 0; i < K; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if(input == 0) {
+                stack.pop();
+            }
+            else {
+                stack.push(input);
+            }
+        }
+        int total = 0;
+        for(int i = 0; i < stack.size(); i++) {
+            total += stack.get(i);
+        }
+        bw.write(String.valueOf(total));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
