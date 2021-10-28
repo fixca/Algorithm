b528529d9e641007f2f7033228043ff7b68ba04e
fixca <41466407+fixca@users.noreply.github.com>
2021-10-28 19:05:46 +0900

    Update 버블정렬.cpp

--- 한국외대/버블정렬.cpp
+++ 한국외대/버블정렬.cpp
@@ -1,1 +1,27 @@
+/*
+    2021/10/28 한국외대 알고리즘 멘토
+    
+    버블정렬(ASC)
+*/
+#include <stdio.h>
+void swap(int* a, int* b);
 
+int main() {
+    int data[5] = {19, 80, 77, 11, 54};
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5 - 1; j++) {
+            if(data[j] > data[j + 1]) {
+                swap(&data[j], &data[j + 1]);
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++) {
+        printf("%d\n", data[i]);
+    }
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
